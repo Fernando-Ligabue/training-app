384eef61225005470be7ed696b8d8888f8035703
Fernando-Ligabue <76406175+Fernando-Ligabue@users.noreply.github.com>
2022-06-29 18:38:02 +0100

    backup project

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+const Footer = () => (
+  <Box mt="80px">
+    <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} fontFamily="Raleway" pt="40px" textAlign="center" pb="40px">Building for FD Web in Bootcamp React June 2022</Typography>
+  </Box>
+);
+
+export default Footer;
